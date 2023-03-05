e4f927d1532866fe36371892ea149baa585feab7
Samsonium <tdesu@vk.com>
2023-03-05 18:43:07 +0300

    Updated token class

--- src/main/java/ru/samsonium/lexer/token/Token.java
+++ src/main/java/ru/samsonium/lexer/token/Token.java
@@ -3,28 +3,44 @@ package ru.samsonium.lexer.token;
 public class Token {
     private final TokenType type;
     private final String    data;
+    private final int       pos;
 
     /**
      * Create token
      * @param type
      * @param data
      */
-    public Token(TokenType type, String data) {
+    public Token(TokenType type, String data, int pos) {
         this.type = type;
         this.data = data;
+        this.pos = pos;
     }
 
-    /**
-     * @return the type
-     */
     public TokenType getType() {
         return type;
     }
 
-    /**
-     * @return the data
-     */
     public String getData() {
         return data;
-    }    
+    }
+    
+    public int getPos() {
+        return pos;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder buff = new StringBuilder();
+        buff.append("ТОКЕН(");
+
+        String d = "тип=" + type.getName() + ",";
+        String d_append = "";
+        if (d.length() < 18)
+            d_append = " ".repeat(18 - d.length());
+
+        buff.append(d + d_append);
+        buff.append("данные=\"" + data + "\")");
+
+        return buff.toString();
+    }
 }
