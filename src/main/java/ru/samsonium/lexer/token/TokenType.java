94a778339f2f1e6934932dd45a5dbced1716d97f
Samsonium <tdesu@vk.com>
2023-03-05 18:43:26 +0300

    switched TokenType enum to class

--- src/main/java/ru/samsonium/lexer/token/TokenType.java
+++ src/main/java/ru/samsonium/lexer/token/TokenType.java
@@ -1,69 +1,26 @@
 package ru.samsonium.lexer.token;
 
-public enum TokenType {
+import java.util.regex.Pattern;
+
+public class TokenType {
+    private final String name;
+    private final String regex;
     
-    // Data types
-    D_Number,
-    D_Hex,
-    D_Char,
-    D_String,
-    D_Bool,
+    public TokenType(String name, String regex) {
+        this.name = name;
+        this.regex = regex;
+    }
 
-    // Binary operators
-    B_Add,
-    B_Sub,
-    B_Mul,
-    B_Div,
-    B_Exp,
-    B_Mod,
+    public String getName() {
+        return name;
+    }
 
-    // Assignment operators
-    As,
-    As_Add,
-    As_Sub,
-    As_Mul,
-    As_Div,
-    As_Exp,
-    As_Mod,
+    public Pattern getRegex() {
+        return Pattern.compile("^" + regex);
+    }
 
-    // Conditional operators
-    C_And,
-    C_Or,
-    C_Neg,
-    C_Eq,
-    C_Gt,
-    C_GtEq,
-    C_Lt,
-    C_LtEq,
-    C_NEq,
-    C_IOf,
-    C_In,
-
-    // Etc operators
-    E_Ret,
-    E_Input,
-    E_Output,
-
-    // Conditional structure
-    CC_If,
-    CC_ElseIf,
-    CC_Else,
-
-    // Cycle structures
-    CF_For,
-    CF_Iter,
-    CF_While,
-    CF_Do,
-
-    // Function structure
-    CFn_Func,
-    CFn_Name,
-    CFn_ArgType,
-    CFn_ArgName,
-    CFn_Type,
-
-    // Etc symbols
-    EC_Semicolon,
-    EC_Comma,
-    EC_EOF
+    @Override
+    public String toString() {
+        return "ТИП(" + name + ")";
+    }
 }
