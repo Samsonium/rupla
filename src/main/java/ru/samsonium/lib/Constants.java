bdf333bb2f58f4bf008042668eb372f00919037b
Samsonium <tdesu@vk.com>
2023-03-06 04:03:30 +0300

    Created Math lib

---
+++ src/main/java/ru/samsonium/lib/Constants.java
@@ -0,0 +1,9 @@
+package ru.samsonium.lib;
+
+public class Constants {
+
+    // MATH
+    public static double PI             = 3.1415926;
+    public static double E              = 2.7182818;
+    public static double GOLDEN_SECTION = 1.6180339;
+}
---
+++ src/main/java/ru/samsonium/lib/stdlib/Math.java
@@ -0,0 +1,9 @@
+package ru.samsonium.lib.stdlib;
+
+import ru.samsonium.lib.Constants;
+
+public class Math {
+    public static double GOLDEN_SECTION = Constants.GOLDEN_SECTION;
+    public static double E = Constants.E;
+    public static double PI = Constants.PI;
+}
