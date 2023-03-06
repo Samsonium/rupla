241ff858c6c85a9378a13e50c42fa70e5f848397
Samsonium <tdesu@vk.com>
2023-03-06 10:54:09 +0300

    Added for statement

--- src/main/java/ru/samsonium/parser/Visitor.java
+++ src/main/java/ru/samsonium/parser/Visitor.java
@@ -1,5 +1,24 @@
 package ru.samsonium.parser;
 
+import ru.samsonium.parser.ast.expressions.BinaryExpression;
+import ru.samsonium.parser.ast.expressions.ConditionalExpression;
+import ru.samsonium.parser.ast.expressions.ValueExpression;
+import ru.samsonium.parser.ast.statements.AssignmentStatement;
+import ru.samsonium.parser.ast.statements.BlockStatement;
+import ru.samsonium.parser.ast.statements.BreakStatement;
+import ru.samsonium.parser.ast.statements.DoWhileStatement;
+import ru.samsonium.parser.ast.statements.ForStatement;
+
 public interface Visitor {
-    // TODO
+    // Statements
+    void visit(AssignmentStatement expression);
+    void visit(BlockStatement expression);
+    void visit(BreakStatement expression);
+    void visit(DoWhileStatement expression);
+    void visit(ForStatement expression);
+
+    // Expressions
+    void visit(BinaryExpression expression);
+    void visit(ConditionalExpression expression);
+    void visit(ValueExpression expression);
 }
